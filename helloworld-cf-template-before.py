094d2212fcb42ec2caef045f89e28911ecd3b1f4
hooneats <hooneats@mz.co.kr>
2022-07-21 13:03:47 +0900

    update

---
+++ helloworld-cf-template-before.py
@@ -0,0 +1,88 @@
+"""클라우드 포메이션 템플릿 생성"""
+from troposphere import (
+    Base64,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template,
+)
+
+ApplicationPort = "3000"
+
+t = Template()
+
+t.set_description("Effective DevOps in AWS: HelloWorld web application")
+
+t.add_parameter(Parameter(
+    "KeyName",
+    Description="Name of an existing EC2 KeyPair to SSH",
+    Type="AWS::EC2::KeyPair::KeyName",
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+
+t.add_parameter(Parameter(
+    "SSHLocation",
+    Description="The IP address range that can be used to SSH to the EC2 instances",
+    Type="String",
+    MinLength="9",
+    MaxLength="18",
+    Default="0.0.0.0/0",
+    AllowedPattern="(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})/(\\d{1,2})",
+    ConstraintDescription="must be a valid IP CIDR range of the form x.x.x.x/x.",
+))
+
+t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp=Ref("SSHLocation"),
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "sudo yum install --enablerepo=epel -y nodejs",
+    "wget http://bit.ly/2vESNuc -O /home/ec2-user/helloworld.js",
+    "wget http://bit.ly/2vVvT18 -O /etc/init/helloworld.conf",
+    "start helloworld"
+]))
+
+t.add_resource(ec2.Instance(
+    "instance",
+    ImageId="ami-cfe4b2b0",
+    InstanceType="t2.micro",
+    SecurityGroups=[Ref("SecurityGroup")],
+    KeyName=Ref("KeyName"),
+    UserData=ud,
+))
+
+t.add_output(Output(
+    "InstancePublicIp",
+    Description="Public IP of our instance.",
+    Value=GetAtt("instance", "PublicIp"),
+))
+
+t.add_output(Output(
+    "WebUrl",
+    Description="Application endpoint",
+    Value=Join("", [
+        "http://", GetAtt("instance", "PublicDnsName"),
+        ":", ApplicationPort
+    ]),
+))
+
+print(t.to_json())
